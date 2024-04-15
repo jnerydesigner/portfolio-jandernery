55b9ac0316768487de5e8f9b1ba872f4c7b65bc3
jnerydesigner <jander.webmaster@gmail.com>
2024-04-15 16:45:42 -0400

    Fix: Redirect create type

--- redirects.ts
+++ redirects.ts
@@ -1,17 +1,15 @@
-import { Redirect as NextRedirect} from 'next';
-
-interface Redirect {
-  source: string;
-  destination: string;
-  permanent: boolean;
-}
-
-const redirects: NextRedirect[] = [
-  {
-    source: '/about',
-    destination: '/',
-    permanent: true,
-  },
-];
-
-export default redirects;
+type NextRedirect<T = Record<string, any>> = T & {
+    source: string;
+    destination: string;
+    permanent: boolean;
+  };
+  
+  const redirects: NextRedirect[] = [
+    {
+      source: '/about',
+      destination: '/',
+      permanent: true,
+    },
+  ];
+  
+  export default redirects;
