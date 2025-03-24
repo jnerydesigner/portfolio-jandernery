ed5d36d7dc30e99ff5a749939ec041a89b4e6f1e
jnerydesigner <jander.webmaster@gmail.com>
2025-03-24 14:56:21 -0400

    Feat: new deploy

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,3 @@
-#!/bin/sh
-echo "Deploy Initialised"
-
-git pull origin main
-
-yarn install --frozen-lockfile
-
-yarn build
-
-pm2 restart portfolio-jandernery 
-
-echo "Deployment Completed"
-
-
-
 
 #!/bin/bash
 set -e # Faz o script parar no primeiro erro
@@ -20,24 +5,27 @@ set -e # Faz o script parar no primeiro erro
 echo "🚀 Iniciando deploy..."
 
 # Atualiza o código
+echo "🚀 Atualizando o código..."
 git pull origin main
 
 # Instala dependências
+echo "🚀 Instalando as Dependencias..."
 yarn install --frozen-lockfile
 
 # Constrói a aplicação
+echo "🚀 Construindo a aplicação..."
 yarn build
 
 # Verifica se o aplicativo já está rodando no PM2
-if pm2 list | grep -q "portfolio-jandernery"; then
+if pm2 list | grep -q "portfolio-jandernery "; then
   echo "🔄 Aplicação encontrada no PM2 - Reiniciando..."
-  pm2 restart portfolio-jandernery || {
+  pm2 restart portfolio-jandernery  || {
     echo "❌ Falha ao reiniciar o PM2"
     exit 1
   }
 else
   echo "⭐ Aplicação não encontrada - Iniciando pela primeira vez..."
-  pm2 start ecosystem.config.cjs || {
+  pm2 start my-pm2-start.json || {
     echo "❌ Falha ao iniciar o PM2"
     exit 1
   }
