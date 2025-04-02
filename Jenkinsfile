3ff40e1734d756502bc9ea28f2e3d3f0036a2558
jnerydesigner <jander.webmaster@gmail.com>
2025-04-02 11:37:09 -0400

    Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+    tools {
+        nodejs 'NodeJS20' // Nome que você configurou no Jenkins
+    }
+    stages {
+        stage('Check Node Version') {
+            steps {
+                script {
+                    def nodeVersion = sh(script: 'node -v', returnStdout: true).trim()
+                    echo "Node.js version: ${nodeVersion}"
+                }
+            }
+        }
+        stage("Deploy Se Liga Dev"){
+            steps {
+                script {
+                    sh './deploy.sh'
+                }
+            }
+        }
+    }
+}
--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,6 @@ set -e # Faz o script parar no primeiro erro
 
 echo "🚀 Iniciando deploy..."
 
-# Atualiza o código
-echo "🚀 Atualizando o código..."
-git pull origin main
 
 # Instala dependências
 echo "🚀 Instalando as Dependencias..."
@@ -16,19 +13,5 @@ yarn install --frozen-lockfile
 echo "🚀 Construindo a aplicação..."
 yarn build
 
-# Verifica se o aplicativo já está rodando no PM2
-if pm2 list | grep -q "portfolio-jandernery "; then
-  echo "🔄 Aplicação encontrada no PM2 - Reiniciando..."
-  pm2 restart portfolio-jandernery  || {
-    echo "❌ Falha ao reiniciar o PM2"
-    exit 1
-  }
-else
-  echo "⭐ Aplicação não encontrada - Iniciando pela primeira vez..."
-  pm2 start my-pm2-start.json || {
-    echo "❌ Falha ao iniciar o PM2"
-    exit 1
-  }
-fi
 
 echo "✅ Deploy concluído com sucesso!"
