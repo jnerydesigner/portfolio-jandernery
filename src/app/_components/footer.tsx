4a11090f066d7f64d0737510b862cd172f72c192
jnerydesigner <jander.webmaster@gmail.com>
2025-05-02 07:00:44 -0400

    Feat: footer

---
+++ src/app/_components/footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <div className="w-full max-w-7xl mx-auto h-12 bg-portfolio-color03 flex justify-center items-center">
+      <div className="w-full max-w-4xl mx-auto h-12 bg-portfolio-color03 flex flex-col justify-center items-center">
+        <p className="text-center text-sm mt-2">
+          © 2025 Se Liga Dev. Todos os direitos reservados.
+        </p>
+      </div>
+    </div>
+  );
+};
--- src/app/components/wrapper-client.tsx
+++ src/app/components/wrapper-client.tsx
@@ -2,12 +2,14 @@
 
 import { AppWrapper } from "@/context/app.context";
 import Header from "./header";
+import { Footer } from "../_components/footer";
 
 export const WrapperClient = ({ children }: { children: React.ReactNode }) => {
   return (
     <AppWrapper>
       <Header />
       {children}
+      <Footer />
     </AppWrapper>
   );
 };
